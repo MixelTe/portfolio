1ba9bd270e3992a468adc56c5b8ee4b00b851d54
Mike <63655075+MixelTe@users.noreply.github.com>
2022-09-27 23:38:46 +0300

    popup for images

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,4 @@
 const filterEls = document.querySelectorAll(".filter input");
-
 filterEls.forEach(el => el.addEventListener("change", onFilter.bind(el, el)))
 
 
@@ -7,4 +6,23 @@ function onFilter(el)
 {
 	const v = el.getAttribute("data-v");
 	console.log(v, el.checked);
+}
+
+
+const popup = document.getElementById("popup");
+const popupImgEl = document.getElementById("popup-img");
+document.getElementById("popup-close").addEventListener("click", popupClose);
+popup.addEventListener("click", e => { if (e.target == popup) popupClose() });
+
+const imgs = document.querySelectorAll(".pimg");
+imgs.forEach(el => el.addEventListener("click", popupImg.bind(el, el)))
+
+function popupImg(img)
+{
+	popup.style.display = "flex";
+	popupImgEl.setAttribute("src", img.getAttribute("src"));
+}
+function popupClose()
+{
+	popup.style.display = "none"
 }
