5bcca22eb68c77e339b92e0e88e8af174906088a
Mike <63655075+MixelTe@users.noreply.github.com>
2022-10-02 01:10:59 +0300

    links to project header

--- scripts/projects.js
+++ scripts/projects.js
@@ -8,3 +8,11 @@ function onFilter(el)
 	console.log(v, el.checked);
 }
 
+const headers = document.querySelectorAll("h1");
+for (const el of headers)
+{
+	const link = document.createElement("a");
+	link.href = `./projects.html#${el.id}`;
+	el.appendChild(link);
+}
+
