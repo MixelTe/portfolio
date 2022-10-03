a864abbb07c2ef108507e13e9d6f742100734858
Mike <63655075+MixelTe@users.noreply.github.com>
2022-10-03 20:57:05 +0300

    project filter

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,13 +1,3 @@
-const filterEls = document.querySelectorAll(".filter input");
-filterEls.forEach(el => el.addEventListener("change", onFilter.bind(el, el)))
-
-
-function onFilter(el)
-{
-	const v = el.getAttribute("data-v");
-	console.log(v, el.checked);
-}
-
 const headers = document.querySelectorAll("h1");
 for (const el of headers)
 {
@@ -16,3 +6,41 @@ for (const el of headers)
 	el.appendChild(link);
 }
 
+
+const filterEls = document.querySelectorAll(".filter input");
+filterEls.forEach(el => el.addEventListener("change", onFilter.bind(el, el)))
+
+const projects = document.querySelectorAll(".project");
+
+
+function onFilter(el)
+{
+	const tags = [];
+	for (const filterEl of filterEls)
+	{
+		const tag = filterEl.getAttribute("data-v");
+		if (filterEl.checked) tags.push(tag);
+	}
+
+	for (const pj of projects)
+	{
+		pj.style.display = "";
+		if (tags.length == 0) continue;
+		
+		const pj_tags = pj.getAttribute("data-tags").split(";");
+		let found = false;
+		for (const pj_tag of pj_tags)
+		{
+			for (const tag of tags)
+			{
+				if (pj_tag == tag)
+				{
+					found = true;
+					break
+				}
+			}
+			if (found) break
+		}
+		if (!found) pj.style.display = "none";
+	}
+}
