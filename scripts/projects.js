82854caf18db33bee6cae19aa3963b7ba543c54b
Mike <63655075+MixelTe@users.noreply.github.com>
2022-11-08 22:49:17 +0300

    update

--- scripts/projects.js
+++ scripts/projects.js
@@ -15,32 +15,20 @@ const projects = document.querySelectorAll(".project");
 
 function onFilter(el)
 {
-	const tags = [];
+	const tag = el.checked ? el.getAttribute("data-v") : null;
 	for (const filterEl of filterEls)
 	{
-		const tag = filterEl.getAttribute("data-v");
-		if (filterEl.checked) tags.push(tag);
+		if (filterEl == el) continue;
+		filterEl.checked = false;
 	}
 
 	for (const pj of projects)
 	{
 		pj.style.display = "";
-		if (tags.length == 0) continue;
-		
+		if (!tag) continue;
+
 		const pj_tags = pj.getAttribute("data-tags").split(";");
-		let found = false;
-		for (const pj_tag of pj_tags)
-		{
-			for (const tag of tags)
-			{
-				if (pj_tag == tag)
-				{
-					found = true;
-					break
-				}
-			}
-			if (found) break
-		}
-		if (!found) pj.style.display = "none";
+		let hasTag = pj_tags.includes(tag);
+		if (!hasTag) pj.style.display = "none";
 	}
 }
